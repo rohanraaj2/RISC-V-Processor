9fe860cd66eaabff4a00cd7f502a29b7585a63f6
Rohan Raj <rohan.raaj2@gmail.com>
2023-05-11 23:19:21 +0500

    files upload

---
+++ Pipelined Processor/Test Bench/testbench.sv
@@ -0,0 +1,32 @@
+module tb_pipelined();
+  reg dclk;
+  reg dreset;
+  
+  Pipelined_Processor r1(dclk, dreset);
+  
+  initial 
+    begin
+    	dclk = 1'b0;
+  end
+  
+  always 
+    begin
+    #5
+    dclk = ~dclk;
+  	end
+  initial 
+    begin
+      dreset = 1'b1;
+      #10
+      dreset = 1'b0;
+      #4000
+      dreset = 1'b1;
+      $finish;
+  	end
+  
+  initial begin
+    $dumpfile("tests.vcd");
+    $dumpvars();
+    #10000 $finish;
+  end
+endmodule
